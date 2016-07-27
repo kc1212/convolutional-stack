a8bd4c506dde070af27545cc750b340282151bd6
kc1212 <kc1212@users.noreply.github.com>
2016-07-27 23:21:20 +1200

    More tests and helper function

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,7 @@ pub fn encode(xs: &Vec<u8>, gss: &Vec<Vec<u8>>) -> Vec<u8> {
         for gs in gss {
             let mut sum = 0;
             for (j, g) in gs.iter().enumerate() {
-                let idx = i as i32 - j as i32;
-                let mut x = 0;
-                if idx >= 0 {
-                    x = xs[idx as usize];
-                }
-                sum ^= g * x;
+                sum ^= g * value_or_0(xs, i, j);
             }
             c.push(sum);
         }
@@ -20,11 +15,20 @@ pub fn encode(xs: &Vec<u8>, gss: &Vec<Vec<u8>>) -> Vec<u8> {
     c
 }
 
+fn value_or_0(xs: &Vec<u8>, i: usize, j: usize) -> u8 {
+    let idx = i as i32 - j as i32;
+    if idx >= 0 {
+        return xs[idx as usize];
+    }
+    0
+}
+
 #[test]
 fn it_works() {
     let xs = vec![1, 0, 1, 1];
-    let g1 = vec![1, 1, 1];
-    let g2 = vec![1, 0, 1];
-    let gss = vec![g1, g2];
-    assert_eq!(encode(&xs, &gss), vec![1, 1, 1, 0, 0, 0, 0, 1]);
+    let gss1 = vec![vec![1, 1, 1], vec![1, 0, 1]];
+    assert_eq!(encode(&xs, &gss1), vec![1, 1, 1, 0, 0, 0, 0, 1]);
+
+    let gss2 = vec![vec![1, 1, 1], vec![1, 1, 0]];
+    assert_eq!(encode(&xs, &gss2), vec![1, 1, 1, 1, 0, 1, 0, 0]);
 }
