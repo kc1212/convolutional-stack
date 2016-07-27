50519ceb29616dc38d30b3231d22c625394d66fe
kc1212 <kc1212@users.noreply.github.com>
2016-07-27 22:57:04 +1200

    Convolutional code encoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
 #![crate_type = "lib"]
 #![crate_name = "convolutional_code"]
 
-pub fn encode_one(xs: &Vec<u8>, gs: &Vec<u8>) -> Vec<u8> {
+pub fn encode(xs: &Vec<u8>, gss: &Vec<Vec<u8>>) -> Vec<u8> {
     let mut c: Vec<u8> = Vec::new();
     for (i, _) in xs.iter().enumerate() {
-        let mut sum = 0;
-        for (j, g) in gs.iter().enumerate() {
-            let idx = i as i32 - j as i32;
-            let mut x = 0;
-            if idx >= 0 {
-                x = xs[idx as usize];
+        for gs in gss {
+            let mut sum = 0;
+            for (j, g) in gs.iter().enumerate() {
+                let idx = i as i32 - j as i32;
+                let mut x = 0;
+                if idx >= 0 {
+                    x = xs[idx as usize];
+                }
+                sum ^= g * x;
             }
-            sum ^= g * x;
+            c.push(sum);
         }
-        c.push(sum);
     }
     c
 }
@@ -21,6 +23,8 @@ pub fn encode_one(xs: &Vec<u8>, gs: &Vec<u8>) -> Vec<u8> {
 #[test]
 fn it_works() {
     let xs = vec![1, 0, 1, 1];
-    let gs = vec![1, 1, 1];
-    assert_eq!(encode_one(&xs, &gs), vec![1, 1, 0, 0]);
+    let g1 = vec![1, 1, 1];
+    let g2 = vec![1, 0, 1];
+    let gss = vec![g1, g2];
+    assert_eq!(encode(&xs, &gss), vec![1, 1, 1, 0, 0, 0, 0, 1]);
 }
