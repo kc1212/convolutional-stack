e93dd8dda4753cb5fe40cefcb43e4a265f29f25e
kc1212 <kc1212@users.noreply.github.com>
2016-09-10 15:29:16 +0200

    Refactored to use DrawingWindow

--- src/main.rs
+++ src/main.rs
@@ -102,19 +102,144 @@ fn max_len(vv: &Vec<cs::CodePath>) -> usize {
     vv.iter().map(|v| v.path.len()).max().unwrap()
 }
 
-struct MainWindow {
-    shared_results: Rc<RefCell<cs::StackResults>>,
-    shared_l: Rc<RefCell<usize>>, // number of decoded bits
+struct DrawingWindow {
     shared_lvl: Rc<RefCell<usize>>,
 }
 
-impl MainWindow {
-    fn new() -> MainWindow {
-        MainWindow {
-            shared_results: Rc::new(RefCell::new(cs::StackResults::new())),
-            shared_l: Rc::new(RefCell::new(0)),
+impl DrawingWindow {
+    fn new() -> DrawingWindow {
+        DrawingWindow {
             shared_lvl: Rc::new(RefCell::new(0)),
         }
+    }
+
+    fn run(&self, parent: &gtk::Window, res: cs::StackResults) {
+
+        // properties derived from results
+        let drawing_w = max_len(&res.paths) * STEP_PX as usize + 100;
+        let max_lvl = res.paths.len();
+        let decoded_l = res.decoded.len() - res.m;
+
+        // widgets
+        let box_popup = gtk::Box::new(Orientation::Vertical, 0);
+        let box_nav = gtk::Box::new(Orientation::Horizontal, 0);
+
+        let drawing = gtk::DrawingArea::new();
+        drawing.set_size_request(drawing_w as i32, 800);
+
+        let btn_next = gtk::Button::new_with_label(">");
+        let btn_back = gtk::Button::new_with_label("<");
+        btn_next.set_halign(Align::Center);
+        btn_back.set_halign(Align::Center);
+
+        // set layout
+        pack_start!(box_nav, true, false => btn_back, btn_next);
+        box_popup.pack_start(&drawing, true, true, 0);
+        box_popup.pack_end(&box_nav, true, true, 0);
+
+        // callbacks
+        let shared_lvl = self.shared_lvl.clone();
+        btn_next.connect_clicked(clone!(drawing => move |_| {
+            let lvl = *shared_lvl.borrow_mut();
+            if lvl < max_lvl {
+                *shared_lvl.borrow_mut() = lvl + 1;
+            }
+            drawing.queue_draw();
+        }));
+
+        let shared_lvl = self.shared_lvl.clone();
+        btn_back.connect_clicked(clone!(drawing => move |_| {
+            let lvl = *shared_lvl.borrow_mut();
+            if lvl > 0 {
+                *shared_lvl.borrow_mut() = lvl - 1;
+            }
+            drawing.queue_draw();
+        }));
+
+        let shared_lvl = self.shared_lvl.clone();
+        drawing.connect_draw(clone!(drawing => move |_, cr| {
+            cr.set_source_rgba(0.0, 0.0, 0.5, 1.0);
+            let h = drawing.get_allocated_height() as f64;
+
+            // show a message if there's nothing to be drawn
+            if *shared_lvl.borrow() == 0 {
+                cr.move_to(0., h / 2.);
+                cr.set_font_size(20.);
+                cr.show_text("Click the '>' button to incrementally draw the tree");
+                cr.move_to(0., h / 2. + 30.);
+                cr.show_text("and the '<' button to step back.");
+                cr.stroke();
+                return Inhibit(false);
+            }
+
+            // do the drawing
+            for i in 0..*shared_lvl.borrow() {
+                let path = &res.paths[i];
+                cr.move_to(0., h / 2.);
+                DrawingWindow::draw_path(cr, h / 2., 1, path.path.clone(), path.mu, decoded_l);
+            }
+
+            Inhibit(false)
+        }));
+
+        // make and show popup
+        let popup = gtk::Window::new(gtk::WindowType::Toplevel);
+        popup.set_modal(true);
+        popup.set_transient_for(Some(parent));
+        popup.set_title("tree");
+        popup.set_border_width(10);
+        popup.add(&box_popup);
+
+        // move to the right of the parent window
+        let (parent_x, parent_y) = parent.get_position();
+        let (parent_w, _) = parent.get_size();
+        popup.move_(parent_x + parent_w, parent_y);
+
+        popup.show_all();
+    }
+
+    fn draw_path(cr: &cairo::Context, h: f64, lvl: usize, mut path: Vec<u8>, mu: f64, l: usize) {
+        if path.is_empty() {
+            cr.rel_move_to(0., -15.); // no need to move back because we're return at the end
+            cr.set_font_size(20.);
+            cr.show_text(&format!("{:.2}", mu));
+            cr.set_line_width(2.);
+            cr.stroke();
+            return
+        }
+
+        let p = path.remove(0);
+        let (x, y) = cr.get_current_point();
+        let h = h / 2.0; // shadow
+        cr.arc(x, y, 5., 0., 2. * ::std::f64::consts::PI);
+        // cr.fill();
+
+        cr.move_to(x, y);
+
+        // draw straight line if we're at the last m positions
+        if lvl > l {
+            cr.rel_line_to(STEP_PX, 0.);
+        }
+        else if p == 0 {
+            cr.rel_line_to(STEP_PX, -h);
+        }
+        else if p == 1 {
+            cr.rel_line_to(STEP_PX, h);
+        }
+        else {
+            panic!("Must be 0 or 1");
+        }
+
+        // recursive step
+        DrawingWindow::draw_path(cr, h, lvl+1, path, mu, l)
+    }
+}
+
+struct MainWindow {}
+
+impl MainWindow {
+    fn new() -> MainWindow {
+        MainWindow {}
     }
 
     fn run(&self) {
@@ -122,12 +247,9 @@ impl MainWindow {
 
         // containers
         let window = gtk::Window::new(gtk::WindowType::Toplevel);
-        let box_main = gtk::Box::new(Orientation::Horizontal, 0);
-        let box_left = gtk::Box::new(Orientation::Vertical, 0);
-        let box_right = gtk::Box::new(Orientation::Vertical, 0);
+        let box_main = gtk::Box::new(Orientation::Vertical, 0);
         let box_tx = gtk::Box::new(Orientation::Horizontal, 0);
         let box_rx = gtk::Box::new(Orientation::Horizontal, 0);
-        let box_nav = gtk::Box::new(Orientation::Horizontal, 0);
         let sep_margin = 20;
 
         // input
@@ -182,35 +304,20 @@ impl MainWindow {
         // start button
         let btn_start = gtk::Button::new_with_label("START");
 
-        // right box
-        let drawing = gtk::DrawingArea::new();
-        drawing.set_size_request(500, 800);
-        drawing.set_margin_left(20);
-
-        let btn_next = gtk::Button::new_with_label(">");
-        let btn_back = gtk::Button::new_with_label("<");
-        btn_next.set_halign(Align::Center);
-        btn_back.set_halign(Align::Center);
-
         // arrange widgets
-        box_main.pack_start(&box_left, false, false, 0);
-        box_main.pack_start(&box_right, true, true, 0);
-        pack_start!(box_left, false, false => lbl_xs, ent_xs, sep_xs);
-        pack_start!(box_left, false, false => lbl_gs, ent_gs, sep_gs);
-        pack_start!(box_left, false, false => lbl_pr, ent_pr, sep_pr);
+        pack_start!(box_main, false, false => lbl_xs, ent_xs, sep_xs);
+        pack_start!(box_main, false, false => lbl_gs, ent_gs, sep_gs);
+        pack_start!(box_main, false, false => lbl_pr, ent_pr, sep_pr);
 
         box_tx.pack_start(&ent_tx, true, true, 0);
         box_tx.pack_end(&btn_tx, false, false, 0);
-        pack_start!(box_left, false, false => lbl_tx, box_tx, sep_tx);
+        pack_start!(box_main, false, false => lbl_tx, box_tx, sep_tx);
 
         box_rx.pack_start(&ent_rx, true, true, 0);
         box_rx.pack_end(&btn_rx, false, false, 0);
-        pack_start!(box_left, false, false => lbl_rx, box_rx, sep_rx);
+        pack_start!(box_main, false, false => lbl_rx, box_rx, sep_rx);
 
-        pack_start!(box_nav, true, false => btn_back, btn_next);
-        box_left.pack_end(&btn_start, false, false, 0);
-        box_right.pack_start(&drawing, true, true, 0);
-        box_right.pack_end(&box_nav, true, true, 0);
+        box_main.pack_end(&btn_start, false, false, 0);
 
         // call backs
         btn_tx.connect_clicked(clone!(ent_xs, ent_gs, ent_tx => move |_| {
@@ -220,8 +327,7 @@ impl MainWindow {
             ent_tx.set_text(&format_bin(&ys));
         }));
 
-        let shared_lvl = self.shared_lvl.clone();
-        btn_rx.connect_clicked(clone!(ent_pr, ent_tx, ent_rx, drawing, window => move |_| {
+        btn_rx.connect_clicked(clone!(ent_pr, ent_tx, ent_rx, window => move |_| {
             let ys = error_dialog!(window, cs::parse_bin(&{
                 match ent_tx.get_text() {
                     Some(x) => x,
@@ -231,81 +337,18 @@ impl MainWindow {
             let pr = error_dialog!(window, cs::parse_pr(&ent_pr.get_buffer().get_text()));
             let noisy_ys = cs::create_noise(&ys, pr);
             ent_rx.set_text(&format_bin(&noisy_ys));
-            *shared_lvl.borrow_mut() = 0; // reset the level because rx is changed
-            drawing.queue_draw();
         }));
 
-        let shared_lvl = self.shared_lvl.clone();
-        ent_rx.connect_changed(clone!(drawing => move |_| {
-            *shared_lvl.borrow_mut() = 0; // reset the level because rx is changed
-            drawing.queue_draw();
-        }));
-
-        let shared_results = self.shared_results.clone();
-        let shared_l = self.shared_l.clone();
-        let shared_lvl = self.shared_lvl.clone();
-        drawing.connect_draw(clone!(drawing => move |_, cr| {
-            let res = shared_results.borrow();
-
-            cr.set_source_rgba(0.0, 0.0, 0.5, 1.0);
-            let w = drawing.get_allocated_width() as f64;
-            let h = drawing.get_allocated_height() as f64;
-
-            // show a message if there's nothing to be drawn
-            if res.m == 0 && res.n == 0 {
-                cr.move_to(w / 2., h / 2.);
-                cr.show_text("Nothing here");
-                cr.stroke();
-                return Inhibit(false);
-            }
-
-            // do the drawing
-            for i in 0..*shared_lvl.borrow() {
-                let path = &res.paths[i];
-                cr.move_to(0., h / 2.);
-                MainWindow::draw_path(cr, h / 2., 1, path.path.clone(), path.mu, *shared_l.borrow());
-            }
-
-            Inhibit(false)
-        }));
-
-        let shared_lvl = self.shared_lvl.clone();
-        let shared_results = self.shared_results.clone();
-        btn_next.connect_clicked(clone!(drawing => move |_| {
-            let lvl = *shared_lvl.borrow_mut();
-            let max_lvl = shared_results.borrow().paths.len();
-            if lvl < max_lvl {
-                *shared_lvl.borrow_mut() = lvl + 1;
-            }
-            drawing.queue_draw();
-        }));
-
-        let shared_lvl = self.shared_lvl.clone();
-        btn_back.connect_clicked(clone!(drawing => move |_| {
-            let lvl = *shared_lvl.borrow_mut();
-            if lvl > 0 {
-                *shared_lvl.borrow_mut() = lvl - 1;
-            }
-            drawing.queue_draw();
-        }));
-
-        let shared_results = self.shared_results.clone();
-        let shared_l = self.shared_l.clone();
-        let shared_lvl = self.shared_lvl.clone();
-        btn_start.connect_clicked(clone!(ent_xs, ent_gs, ent_pr, ent_rx, drawing, window => move |_| {
+        btn_start.connect_clicked(clone!(ent_xs, ent_gs, ent_pr, ent_rx, window => move |_| {
             let xs = ent_xs.get_buffer().get_text();
             let gs = ent_gs.get_buffer().get_text();
             let pr = ent_pr.get_buffer().get_text();
             let rx = ent_rx.get_buffer().get_text();
 
             let res = error_dialog!(window, run_stack_algo(&xs, &gs, &pr, &rx));
-            let w = max_len(&res.paths) * STEP_PX as usize + 50;
 
-            drawing.set_size_request(w as i32, 800);
-            *shared_l.borrow_mut() = res.decoded.len() - res.m;
-            *shared_results.borrow_mut() = res;
-            *shared_lvl.borrow_mut() = 0;
-            drawing.queue_draw();
+            let dw = DrawingWindow::new();
+            dw.run(&window, res); // blocks?
         }));
 
         // main window
@@ -322,42 +365,6 @@ impl MainWindow {
         window.add(&box_main);
         window.show_all();
         gtk::main();
-    }
-
-    fn draw_path(cr: &cairo::Context, h: f64, lvl: usize, mut path: Vec<u8>, mu: f64, l: usize) {
-        if path.is_empty() {
-            cr.rel_move_to(0., -15.); // no need to move back because we're return at the end
-            cr.set_font_size(20.);
-            cr.show_text(&format!("{:.2}", mu));
-            cr.set_line_width(2.);
-            cr.stroke();
-            return
-        }
-
-        let p = path.remove(0);
-        let (x, y) = cr.get_current_point();
-        let h = h / 2.0; // shadow
-        cr.arc(x, y, 5., 0., 2. * ::std::f64::consts::PI);
-        // cr.fill();
-
-        cr.move_to(x, y);
-
-        // draw straight line if we're at the last m positions
-        if lvl > l {
-            cr.rel_line_to(STEP_PX, 0.);
-        }
-        else if p == 0 {
-            cr.rel_line_to(STEP_PX, -h);
-        }
-        else if p == 1 {
-            cr.rel_line_to(STEP_PX, h);
-        }
-        else {
-            panic!("Must be 0 or 1");
-        }
-
-        // recursive step
-        MainWindow::draw_path(cr, h, lvl+1, path, mu, l)
     }
 }
 
